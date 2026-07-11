900bf2888f9f99f213a0e8ffbaa481959c9c9f14
Mohammad Anas Hannan <mohammadanashannan@gmail.com>
2026-07-11 17:43:54 +0530

    Week 4 Day 4 done

--- Week_4/Day_4/PathSum.cpp
+++ Week_4/Day_4/PathSum.cpp
@@ -14,6 +14,7 @@ public:
     bool hasPathSum(TreeNode* root, int targetSum) {
         if(!root) return false;
         if(!root->left && !root->right) return targetSum==root->val;
+        
         return hasPathSum(root->left,targetSum-root->val) || hasPathSum(root->right,targetSum-root->val);
     }
 };
